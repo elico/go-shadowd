ec5d0a39cd43b007365bf38509e8691dd0c05e6e
Eliezer Croitoru <eliezer@ngtech.co.il>
2016-01-28 05:00:35 +0200

    Fixed couple typos and added some documentation

--- go-shadowd.go
+++ go-shadowd.go
@@ -1,19 +1,21 @@
 package shadowd
 
 import (
+	"bufio"
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net"
 	"net/http"
-	"encoding/hex"
-	"encoding/json"
 	"strings"
-	"bytes"
-	"bufio"
 )
 
+// Global default settings constants
+// All STATUS_X are describing an analysis result.
 const (
 	SHADOWD_CONNECTOR_VERSION        = "2.0.1-go"
 	SHADOWD_CONNECTOR_CONFIG         = "/etc/shadowd/go-connector.ini"
@@ -29,135 +31,147 @@ const (
 
 var VERSION = "2.0.1"
 
+// A shadowd connection, this allows to send a request for analysis.
 type ShadowdConn struct {
 	ServerAddr string
-	ReadBody bool
-	ProfileId string
+	ReadBody   bool
+	ProfileId  string
 	ProfileKey string
-	Logfile string
-	Debug bool
+	Logfile    string
+	Debug      bool
 }
 
-
-func escapeKey(key string)string{
-	newstr := strings.Replace(key, "/", "\\/" ,-1)
-	newstr = strings.Replace(newstr, "|", "\\|",-1)
+func escapeKey(key string) string {
+	newstr := strings.Replace(key, "/", "\\/", -1)
+	newstr = strings.Replace(newstr, "|", "\\|", -1)
 	return newstr
 }
 
-func unescapeKey(key string)string{
-	newstr := strings.Replace(key,"\\\\","\\",-1)
-	newstr =  strings.Replace(newstr, "\\|","|",-1)
+func unescapeKey(key string) string {
+	newstr := strings.Replace(key, "\\\\", "\\", -1)
+	newstr = strings.Replace(newstr, "\\|", "|", -1)
 	return newstr
 }
 
-func (serverconn *ShadowdConn)SendToShadowd(req *http.Request)(string, error){
-		newmap := make(map[string]interface{})
-		inputmap := make(map[string]string)
-		newmap["version"] = SHADOWD_CONNECTOR_VERSION
-		newmap["client_ip"] = strings.Split(req.RemoteAddr, ":")[0]
-		newmap["caller"] = req.URL.Path
-		newmap["resource"] = req.URL.Path
-		
-		inputmap["SERVER|HTTP_REMOTEADDR"] = req.RemoteAddr
-		
-		for k, v := range req.URL.Query() {
-			for _, s := range v {
-				inputmap[req.Method+"|"+escapeKey(k)] = s
-			}
-		}
+// Sends an http request to processing on the shadowd service.
+// Returns a json object in a string format with the analysis result.
+// The json "status" key is an integer(float64 when json parses it) that
+// reflect the result of the test ranging from 1 to 6.
+// 1 means that it is ok to pass the request to the origin server
+// 5 and 6 means that the requester or the request was identified as an attack.
+// The error is always nil unless some special parsing or communication happen.
+// It is up to the developer what to do for each error and status code.
+func (serverconn *ShadowdConn) SendToShadowd(req *http.Request) (string, error) {
+	newmap := make(map[string]interface{})
+	inputmap := make(map[string]string)
+	newmap["version"] = SHADOWD_CONNECTOR_VERSION
+	newmap["client_ip"] = strings.Split(req.RemoteAddr, ":")[0]
+	newmap["caller"] = req.URL.Path
+	newmap["resource"] = req.URL.Path
 
-		coockie := req.Cookies()
-		for _, v := range coockie {
-			inputmap["COOKIE|" + strings.ToUpper(v.Name)] = v.Value
-		}
+	inputmap["SERVER|HTTP_REMOTEADDR"] = req.RemoteAddr
 
-		inputmap["SERVER|HTTP_COOKIE"] = ""
-		for _, v := range coockie {
-			inputmap["SERVER|HTTP_COOKIE"] = inputmap["SERVER|HTTP_COOKIE"] + v.Name +"="+v.Value+"; "
+	for k, v := range req.URL.Query() {
+		for _, s := range v {
+			inputmap[req.Method+"|"+escapeKey(k)] = s
 		}
-		if len(inputmap["SERVER|HTTP_COOKIE"]) == 0 {
-			delete(inputmap,"SERVER|HTTP_COOKIE")
-		}
+	}
 
-		headers := req.Header
-		for k, v := range headers {
-			inputmap["SERVER|HTTP_" + strings.Replace(strings.ToUpper(k), "-","_", -1)] = strings.Join(v, "")
-		}
+	coockie := req.Cookies()
+	for _, v := range coockie {
+		inputmap["COOKIE|"+strings.ToUpper(v.Name)] = v.Value
+	}
 
-		if req.Method != "GET" && serverconn.ReadBody {
-			contents, err := ioutil.ReadAll(req.Body)
-			if err != nil {
-				return "", err
-			} else {
-				inputmap["DATA|raw"] = string(contents)
-				// Ne need to make the body readable again
-				req.Body = ioutil.NopCloser(bytes.NewBufferString(inputmap["DATA|raw"]))
-			}
-		}
-		host, port, err := net.SplitHostPort(req.Host)
+	inputmap["SERVER|HTTP_COOKIE"] = ""
+	for _, v := range coockie {
+		inputmap["SERVER|HTTP_COOKIE"] = inputmap["SERVER|HTTP_COOKIE"] + v.Name + "=" + v.Value + "; "
+	}
+	if len(inputmap["SERVER|HTTP_COOKIE"]) == 0 {
+		delete(inputmap, "SERVER|HTTP_COOKIE")
+	}
+
+	headers := req.Header
+	for k, v := range headers {
+		inputmap["SERVER|HTTP_"+strings.Replace(strings.ToUpper(k), "-", "_", -1)] = strings.Join(v, "")
+	}
+
+	if req.Method != "GET" && serverconn.ReadBody {
+		contents, err := ioutil.ReadAll(req.Body)
 		if err != nil {
-			fmt.Println("Error parsing server host+port", err)
-			//return "", err
-		}
-		inputmap["SERVER|HTTP_HOST"] = host
-		inputmap["SERVER|HTTP_PORT"] = port
-		newmap["input"] = inputmap
-
-		jsonData, err := json.Marshal(inputmap)
-		if err != nil {
-			fmt.Println("JSON marshaling failed:", err)
 			return "", err
+		} else {
+			inputmap["DATA|raw"] = string(contents)
+			// Ne need to make the body readable again
+			req.Body = ioutil.NopCloser(bytes.NewBufferString(inputmap["DATA|raw"]))
 		}
-		
-		hash := make(map[string]string)
-		
-		//mac := hmac.New(sha256.New, nil)
-		//mac.Write([]byte(unescapeKey(string(jsonData))))
-		//expectedMAC := hex.EncodeToString(mac.Sum(nil))
-		//hash["sha256"] = expectedMAC
-		
-		newmap["hashes"] = hash
-		
-		jsonData, err = json.Marshal(newmap)
-		if err != nil {
-			fmt.Println("JSON marshaling failed:", err)
-			return "", err
-		}
-		
-		mac := hmac.New(sha256.New, []byte(serverconn.ProfileKey))
-		newjson := []byte(unescapeKey(string(jsonData)))
-		mac.Write(newjson)
-		expectedMAC := hex.EncodeToString(mac.Sum(nil))
-		if serverconn.Debug {
-			fmt.Printf("%v\n%v\n%v\n", serverconn.ProfileId, expectedMAC, string(newjson))
-		}
-		//send the fomratted string into the shadowd server at port 9115
+	}
+	host, port, err := net.SplitHostPort(req.Host)
+	if err != nil {
+		fmt.Println("Error parsing server host+port", err)
+		//return "", err
+	}
+	inputmap["SERVER|HTTP_HOST"] = host
+	inputmap["SERVER|HTTP_PORT"] = port
+	newmap["input"] = inputmap
 
-		tcpAddr, err := net.ResolveTCPAddr("tcp", serverconn.ServerAddr)
-		if err != nil {
-			println("ResolveTCPAddr failed:", err.Error())
-			return "", err
-		}
-		conn, err := net.DialTCP("tcp", nil, tcpAddr)
-		if err != nil {
-			println("Couldn't connect to server:", tcpAddr, err.Error())
-			return "", err
-		}		
-		
-		// Sending data to the server
-		fmt.Fprintf(conn, "%s\n%s\n%s\n", serverconn.ProfileId, expectedMAC, string(jsonData))
-		
-		reader := bufio.NewReader(conn)
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			println("Read from server failed:", err.Error())
-			return "", err
-		}
-		
-		if serverconn.Debug {
-			fmt.Println("reply from server=", line)
-		}
-		conn.Close()
-		return line, nil
+	jsonData, err := json.Marshal(inputmap)
+	if err != nil {
+		fmt.Println("JSON marshaling failed:", err)
+		return "", err
+	}
+
+	hash := make(map[string]string)
+
+	//mac := hmac.New(sha256.New, nil)
+	//mac.Write([]byte(unescapeKey(string(jsonData))))
+	//expectedMAC := hex.EncodeToString(mac.Sum(nil))
+	//hash["sha256"] = expectedMAC
+
+	newmap["hashes"] = hash
+
+	jsonData, err = json.Marshal(newmap)
+	if err != nil {
+		fmt.Println("JSON marshaling failed:", err)
+		return "", err
+	}
+
+	mac := hmac.New(sha256.New, []byte(serverconn.ProfileKey))
+	newjson := []byte(unescapeKey(string(jsonData)))
+	mac.Write(newjson)
+	expectedMAC := hex.EncodeToString(mac.Sum(nil))
+	if serverconn.Debug {
+		fmt.Printf("%v\n%v\n%v\n", serverconn.ProfileId, expectedMAC, string(newjson))
+	}
+	//send the fomratted string into the shadowd server at port 9115
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", serverconn.ServerAddr)
+	if err != nil {
+		fmt.Println("ResolveTCPAddr failed:", err.Error())
+		return "", err
+	}
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		fmt.Println("Couldn't connect to server:", tcpAddr, err.Error())
+		return "", err
+	}
+	defer conn.Close()
+
+	// Sending data to the server
+	_, err = fmt.Fprintf(conn, "%s\n%s\n%s\n", serverconn.ProfileId, expectedMAC, string(jsonData))
+	if err != nil {
+		fmt.Println("Error writing to the server:", err.Error())
+		return "", err
+	}
+
+	reader := bufio.NewReader(conn)
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading from the server:", err.Error())
+		return "", err
+	}
+
+	if serverconn.Debug {
+		fmt.Println("reply from server=", line)
+	}
+	return line, nil
 }
